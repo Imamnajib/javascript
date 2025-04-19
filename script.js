cbf071d8cbd5567798bea89db73a56573f5b6216
muhammadimamnajib <136040074+Imamnajib@users.noreply.github.com>
2025-04-19 11:39:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// var nama =  prompt('masukan nama :'+nama);
+// alert(nama);
+
+// var nama = confirm('apakah anda yakin ');
+// alert(nama);
+
+// var tes = confirm('kamu yakin')
+//     if(tes === true){
+//         alert('user menekan tombol ok')
+//     }else{
+//         alert('user menekan tombol cancel')
+//     }
+
+alert('selamat datang')
+
+var lagi = true
+while(lagi){
+    var nama = prompt('masukan nama ')
+    alert('halo' + nama);
+
+    lagi = confirm('coba lagi ??');
+}
+alert('selamat tinggal');
